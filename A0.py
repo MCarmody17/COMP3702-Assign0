86ef90fe086df38314d6a409221a2bd31ac2276b
MCarmody <71148703+MCarmody17@users.noreply.github.com>
2022-08-07 17:17:34 +0200

    commit 4

--- A0.py
+++ A0.py
@@ -8,54 +8,51 @@ Answer each question in the corresponding method definition stub below
 """
 
 
-def Q1(A,B):
-    union = set(A|B)
+def Q1(A, B):
+    union = set(A | B)
     intersection = set(A & B)
-    return union,intersection
+    return union, intersection
 
 
-def Q2(A,B):
-    intersection = set(A&B)
+def Q2(A, B):
+    intersection = set(A & B)
     if(len(intersection) == 0):
         return 'DISJOINT'
-    
+
     return 'INTERSECTING'
 
 
-
-def Q3(a,b):
+def Q3(a, b):
     X = set()
     Y = set()
     G = set()
-    for x in range(0,a):
+    for x in range(0, a):
         X.add(x)
 
-    for y in range(0,b):
+    for y in range(0, b):
         Y.add(y)
 
     for x, y in zip(X, Y):
         G.add(x)
         G.add(y)
 
-    return X,Y,G
+    return X, Y, G
 
 
-def Q4(E,n):
+def Q4(E, n):
     n_successors = []
-    n_item
     for item in E.items():
         if item.contains(n):
             n_item = item.value().split(',')
             n_successors.add(n_item(1))
-            
-   
+
     return n_successors
 
 
-def Q5(inFile,outFile,remove):
+def Q5(inFile, outFile, remove):
     print('Character '+remove+' removed from '+inFile)
     print('Output written to '+outFile)
 
 
-def Q6(state1,state2):
+def Q6(state1, state2):
     print('IMPOSSIBLE')
