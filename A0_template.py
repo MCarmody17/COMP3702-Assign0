93088158ce1ace167b1b5a1f61a29a63c3cb9498
MCarmody <71148703+MCarmody17@users.noreply.github.com>
2022-08-07 14:23:46 +0200

    first init

---
+++ A0_template.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Assignment 0 template
+
+For submission, rename this file to "A0.py" 
+
+Answer each question in the corresponding method definition stub below
+"""
+
+
+def Q1(A,B):
+    union = set()
+    intersection = set()
+    return union,intersection
+
+
+def Q2(A,B):
+    return 'DISJOINT'
+
+
+def Q3(a,b):
+    X = set()
+    Y = set()
+    G = set()
+    return X,Y,G
+
+
+def Q4(E,n):
+    n_successors = []
+    return n_successors
+
+
+def Q5(inFile,outFile,remove):
+    print('Character '+remove+' removed from '+inFile)
+    print('Output written to '+outFile)
+
+
+def Q6(state1,state2):
+    print('IMPOSSIBLE')
