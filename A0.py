155ab86a3073e1adcfef1ba1e4325680f09ed194
MCarmody <71148703+MCarmody17@users.noreply.github.com>
2022-08-11 00:01:17 +0200

    Q5

--- A0.py
+++ A0.py
@@ -70,4 +70,28 @@ def Q5(inFile, outFile, remove):
 
 
 def Q6(state1, state2):
+
+    counter1 = 0
+    counter2 = 0
+    for ch in state1:
+        if ch == "_":
+            for chars in state2:
+                if chars == "_":
+                    '''
+                    if abs(counter1 - counter2) != 1 or abs(counter1 - counter2) != 3:
+                        print('IMPOSSIBLE')
+                    else:
+                        '''
+                    if(counter1 - counter2 == 1):
+                        print('L')
+                    if(counter1 - counter2 == -1):
+                        print('R')
+                    if(counter1 - counter2 == 3):
+                        print('U')
+                    if(counter1 - counter2 == -3):
+                        print('D')
+
+                counter2 += 1
+        counter1 += 1
+
     print('IMPOSSIBLE')
